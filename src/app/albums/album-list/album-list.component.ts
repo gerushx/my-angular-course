e65458677bd0e4630a268118324020e70059daca
gerushx <georushx@gmail.com>
2019-05-17 08:55:10 -0400

    chapter9

--- src/app/albums/album-list/album-list.component.ts
+++ src/app/albums/album-list/album-list.component.ts
@@ -1,30 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { AlbumService } from "../shared/album.service";
 import { Album } from "../album.model";
-import { ALBUMS } from "../albums.data";
-import { AlbumService } from '../shared/album.service';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-album-list',
-  templateUrl: './album-list.component.html',
-  styleUrls: ['./album-list.component.css']
+  selector: "app-album-list",
+  templateUrl: "./album-list.component.html",
+  styleUrls: ["./album-list.component.css"]
 })
 export class AlbumListComponent implements OnInit {
-  albumsArray: Album[];
+  albumsArray: Observable<Album[]>;
 
   constructor(private albumService: AlbumService) { }
 
-  getAlbums() {
-    this.albumsArray = this.albumService.getAlbums();
-  }
-
-
   ngOnInit(): void {
-
-      this.getAlbums();
-  }
+    this.getAlbums();
+   }
 
   parentFunctionHandler(album) {
     alert('Album ' + album.albumName + ' was sent from the album card component');
-}
-
+  }
+  getAlbums() {
+    this.albumsArray = this.albumService.getAlbums();
+  }
 }
